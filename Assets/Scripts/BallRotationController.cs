f3e638d01b6a6eddddbe2f92fc37cbc7bc8ff104
gueguet <gaetan.verdinpol@gmail.com>
2018-11-27 16:26:54 +0100

    first commit of the dev app

---
+++ Assets/Scripts/BallRotationController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallRotationController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.Rotate(0, Time.deltaTime, 0, Space.World);
+
+    }
+}
---
+++ Assets/Scripts/SharingenRotation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SharingenRotation : MonoBehaviour {
+
+    public GameObject ball;
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+
+        //var plane = new Plane(gameObject.transform.position.normalized, 10);
+
+        transform.Rotate(transform.up * Time.deltaTime * 1000, Space.World);
+
+        transform.RotateAround(ball.transform.position, transform.up, Time.deltaTime * 250);
+		
+	}
+}
